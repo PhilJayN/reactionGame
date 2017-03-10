e8d4f04ccb201a30a67c16638f70091d8399eeb4
Philip Nguyen <phi.nguyen@laverne.edu>
2017-03-09 17:30:48 -0800

    Add on click start btn

--- js/boxGame.js
+++ js/boxGame.js
@@ -1,1 +1,38 @@
+// var Module = (function () {
+//
+//   var privateArray = [];
+//
+//   var publicMethod = function (somethingOfInterest) {
+//     privateArray.push(somethingOfInterest);
+// 		console.log('private arr is now', privateArray);
+//   };
+//
+//   return {
+//     publicMethod: publicMethod
+//   };
+//
+// })();
+//
+// Module.publicMethod('text');
 
+
+// var $card = $('.card');
+// $card.off().on('click', function() {
+
+
+var App = (function () {
+
+  
+  var $startBtn = $('.start-btn');
+  $startBtn.on('click', function () {
+    console.log('clicked');
+  });
+
+  var publicMethod = function (somethingOfInterest) {
+  };
+
+  return {
+    publicMethod: publicMethod
+  };
+
+})();
