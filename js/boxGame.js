587ce957198f3482fa57d5c78717b333d6d7c12a
Philip Nguyen <phi.nguyen@laverne.edu>
2017-03-09 18:24:28 -0800

    Add basic box placement position

--- js/boxGame.js
+++ js/boxGame.js
@@ -20,29 +20,47 @@
 // $card.off().on('click', function() {
 
 
-var App = (function () {
+var App = {
 
-  var publicMethod = function (somethingOfInterest) {
-  };
 
-  var createDivSize = function () {
+
+  init: function () {
+    this.createDivSize();
+    console.log('asdfjk');
+  },
+
+
+
+  createDivSize: function () {
     var divSize = Math.random() * 100;
-  };
+    // return divSize;
+  },
 
-  var createPos = function () {
-    // var posx = ()
-  };
+  generatePosition: function () {
+    var docWidth = $(document).width();
+    var docHeight = $(document).height();
+    var posx = (Math.random() * 234);
+    var posy = (Math.random() * 234);
+    console.log(posx);
+  },
 
+  placement: function () {
+    var $bullseye = $('.bullseye');
+    $bullseye.css({
+      'background-color': 'blue',
+      'position': 'absolute',
+      'left': '344px',
+      'top': '344'
+    });
+  },
 
-  var handlers = (function () {
+  handlers: (function () {
     var $startBtn = $('.start-btn');
     $startBtn.on('click', function () {
       console.log('clicked');
     });
-  })();
+  })()
 
-  return {
-    publicMethod: publicMethod
-  };
+};
 
-})();
+App.init();
